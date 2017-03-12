c92c1107fa998adcaf7e66876978465b827e815f
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2017-03-12 18:34:46 -0500

    add v2.4 String#Floor

--- lib/polyfill/v2_4/integer.rb
+++ lib/polyfill/v2_4/integer.rb
@@ -1,11 +1,13 @@
 require 'polyfill/v2_4/integer/ceil'
 require 'polyfill/v2_4/integer/digits'
+require 'polyfill/v2_4/integer/floor'
 
 module Polyfill
   module V2_4
     module Integer
       include Ceil
       include Digits
+      include Floor
     end
   end
 end
---
+++ spec/polyfill/v2_4/integer/floor_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe 'Integer#floor' do
+  using Polyfill::V2_4::Integer::Floor
+
+  context 'existing behavior' do
+    it 'returns itself' do
+      expect(1.floor).to eql 1
+    end
+  end
+
+  context '2.4' do
+    it 'returns itself when called with 0' do
+      expect(1.floor(0)).to eql 1
+    end
+
+    it 'returns a float of the number when called with > 0' do
+      expect(1.floor(1)).to eql 1.0
+    end
+
+    it 'calls to_int on anything passed' do
+      value = double('value')
+      allow(value).to receive(:to_int).and_return(1)
+      expect(1.floor(value)).to eql 1.0
+    end
+
+    it 'floors up when called with < 0' do
+      expect(15.floor(-1)).to eql 10
+      expect(15.floor(-2)).to eql 0
+    end
+  end
+end

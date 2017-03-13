a037eb700b783650287e575aad314d68e54d7f2f
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2017-03-12 22:03:08 -0500

    check responds_to? for Numeric#finite? and Numeric#infinite?

--- spec/polyfill/v2_4/numeric/finite__q_spec.rb
+++ spec/polyfill/v2_4/numeric/finite__q_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe 'Numeric#finite?' do
   using Polyfill::V2_4::Numeric::Finite__Q
 
+  it 'responds to finite?' do
+    expect(1.respond_to?(:finite?)).to be true
+  end
+
   it 'returns true if a number is finite' do
     expect(1.finite?).to be true
     expect(1i.finite?).to be true
--- spec/polyfill/v2_4/numeric/infinite__q_spec.rb
+++ spec/polyfill/v2_4/numeric/infinite__q_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe 'Numeric#infinite?' do
   using Polyfill::V2_4::Numeric::Infinite__Q
 
+  it 'responds to infinite?' do
+    expect(1.respond_to?(:infinite?)).to be true
+  end
+
   it 'returns nil if a number is not infinite' do
     expect(1.infinite?).to be nil
     expect(1i.infinite?).to be nil

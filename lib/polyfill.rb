f4e9d6a406d4ea823c62a63e1a12ad97834758bc
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2017-04-18 21:48:12 -0500

    adding some comments for clarification

--- lib/polyfill.rb
+++ lib/polyfill.rb
@@ -10,6 +10,9 @@ end
 def Polyfill(options = {}) # rubocop:disable Style/MethodName
   mod = Module.new
 
+  #
+  # parse options
+  #
   objects, others = options.partition { |key,| key[/\A[A-Z]/] }
   others = others.to_h
 
@@ -32,9 +35,7 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
     raise ArgumentError, "unknown keyword: #{others.first[0]}"
   end
 
-  current_ruby_version = RUBY_VERSION[/\A(\d+\.\d+)/, 1]
-  all_instance_modules = []
-
+  # if no objects are given we need them all
   if objects.empty?
     objects = versions.each_with_object({}) do |(_, version_module), acc|
       version_module.constants(false).each do |obj_name|
@@ -43,7 +44,16 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
     end
   end
 
+  #
+  # useful vars
+  #
+  current_ruby_version = RUBY_VERSION[/\A(\d+\.\d+)/, 1]
+  all_instance_modules = []
+
   objects.each do |full_name, methods|
+    #
+    # find all polyfills for the object across all versions
+    #
     object_module_names = full_name.to_s.split('::')
 
     object_modules = versions
@@ -65,6 +75,9 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
       raise ArgumentError, %Q("#{full_name}" is not a valid class or has no updates)
     end
 
+    #
+    # get all class modules and instance modules from the polyfills
+    #
     class_modules = object_modules.map do |version_number, object_module|
       begin
         [version_number, object_module.const_get(:ClassMethods, false).clone]
@@ -76,12 +89,15 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
       [version_number, object_module.clone]
     end
 
+    #
+    # get all requested class and instance methods
+    #
     if methods != :all && (method_name = methods.find { |method| method !~ /\A[.#]/ })
       raise ArgumentError, %Q("#{method_name}" must start with a "." if it's a class method or "#" if it's an instance method)
     end
 
     all_methods_for = lambda do |modules|
-      modules.flat_map { |_, class_module| class_module.instance_methods }.uniq
+      modules.flat_map { |_, m| m.instance_methods }.uniq
     end
     available_class_methods = all_methods_for.call(class_modules)
     available_instance_methods = all_methods_for.call(instance_modules)
@@ -89,16 +105,19 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
     select_and_clean = lambda do |leader|
       methods.select { |method| method.start_with?(leader) }.map { |method| method[1..-1].to_sym }
     end
-    class_methods = methods == :all ? available_class_methods : select_and_clean.call('.')
-    instance_methods = methods == :all ? available_instance_methods : select_and_clean.call('#')
+    requested_class_methods = methods == :all ? available_class_methods : select_and_clean.call('.')
+    requested_instance_methods = methods == :all ? available_instance_methods : select_and_clean.call('#')
 
-    unless (leftovers = (class_methods - available_class_methods)).empty?
+    unless (leftovers = (requested_class_methods - available_class_methods)).empty?
       raise ArgumentError, %Q(".#{leftovers.first}" is not a valid method on #{full_name} or has no updates)
     end
-    unless (leftovers = (instance_methods - available_instance_methods)).empty?
+    unless (leftovers = (requested_instance_methods - available_instance_methods)).empty?
       raise ArgumentError, %Q("##{leftovers.first}" is not a valid method on #{full_name} or has no updates)
     end
 
+    #
+    # get the class(es) to refine
+    #
     base_class = object_modules.first.last.name.sub(/\APolyfill::V\d_\d::/, '')
     base_classes =
       case base_class
@@ -112,14 +131,17 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
         [base_class]
       end
 
+    #
+    # refine in class methods
+    #
     class_modules.each do |version_number, class_module|
       next if version_number <= current_ruby_version
 
       class_module.instance_methods.each do |name|
-        class_module.send(:remove_method, name) unless class_methods.include?(name)
+        class_module.send(:remove_method, name) unless requested_class_methods.include?(name)
       end
 
-      mod.module_exec(class_methods) do |methods_added|
+      mod.module_exec(requested_class_methods) do |methods_added|
         base_classes.each do |klass|
           refine Object.const_get(klass).singleton_class do
             include class_module
@@ -145,16 +167,19 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
       end
     end
 
+    #
+    # refine in instance methods
+    #
     instance_modules.each do |version_number, instance_module|
       next if version_number <= current_ruby_version
 
       instance_module.instance_methods.each do |name|
-        instance_module.send(:remove_method, name) unless instance_methods.include?(name)
+        instance_module.send(:remove_method, name) unless requested_instance_methods.include?(name)
       end
 
       all_instance_modules << instance_module
 
-      mod.module_exec(instance_methods) do |methods_added|
+      mod.module_exec(requested_instance_methods) do |methods_added|
         base_classes.each do |klass|
           refine Object.const_get(klass) do
             include instance_module
@@ -181,6 +206,9 @@ def Polyfill(options = {}) # rubocop:disable Style/MethodName
     end
   end
 
+  #
+  # make sure the includes get added if this module is included
+  #
   mod.singleton_class.send(:define_method, :included) do |base|
     all_instance_modules.each do |instance_module|
       base.include instance_module

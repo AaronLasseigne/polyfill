30e330de8e730eba1068b217cc99cccd26a70d15
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2018-12-29 22:37:11 -0600

    fix v2.6 Hash#merge with a block

--- lib/polyfill/v2_6/hash.rb
+++ lib/polyfill/v2_6/hash.rb
@@ -2,8 +2,16 @@ module Polyfill
   module V2_6
     module Hash
       def merge(*args)
-        args.each_with_object(dup) do |arg, h|
-          h.merge!(arg)
+        if block_given?
+          block = ::Proc.new
+
+          args.each_with_object(dup) do |arg, h|
+            h.merge!(arg, &block)
+          end
+        else
+          args.each_with_object(dup) do |arg, h|
+            h.merge!(arg)
+          end
         end
       end
     end
--- spec/polyfill/v2_6/hash/instance/merge_spec.rb
+++ spec/polyfill/v2_6/hash/instance/merge_spec.rb
@@ -17,4 +17,69 @@ RSpec.describe 'Hash#merge' do
     expect(h.merge(h1)).to eql h1
     expect(h.merge(h1, h2)).to eql h1.merge(h2)
   end
+
+  it 'returns a new hash by combining self with the contents of other' do
+    h = { 1 => :a, 2 => :b, 3 => :c }.merge(a: 1, c: 2)
+    expect(h).to eql(c: 2, 1 => :a, 2 => :b, a: 1, 3 => :c)
+
+    hash = { a: 1, b: 2 }
+    expect({}.merge(hash)).to eql hash
+    expect(hash.merge({})).to eql hash
+
+    h = { 1 => :a, 2 => :b, 3 => :c }.merge(1 => :b)
+    expect(h).to eql(1 => :b, 2 => :b, 3 => :c)
+
+    h = { 1 => :a, 2 => :b }.merge(1 => :b, 3 => :c)
+    expect(h).to eql(1 => :b, 2 => :b, 3 => :c)
+  end
+
+  it 'sets any duplicate key to the value of block if passed a block' do
+    h1 = { a: 2, b: 1, d: 5 }
+    h2 = { a: -2, b: 4, c: -3 }
+    r = h1.merge(h2) { |_k, _x, _y| nil }
+    expect(r).to eql(a: nil, b: nil, c: -3, d: 5)
+
+    r = h1.merge(h2) { |k, x, y| "#{k}:#{x + 2 * y}" }
+    expect(r).to eql(a: 'a:-2', b: 'b:9', c: -3, d: 5)
+
+    expect do
+      h1.merge(h2) { |_k, _x, _y| raise(IndexError) }
+    end.to raise_error(IndexError)
+
+    r = h1.merge(h1) { |_k, _x, _y| :x }
+    expect(r).to eql(a: :x, b: :x, d: :x)
+  end
+
+  it 'tries to convert the passed argument to a hash using #to_hash' do
+    obj = double('{1=>2}')
+    allow(obj).to receive(:to_hash).and_return(1 => 2)
+
+    expect({ 3 => 4 }.merge(obj)).to eql(1 => 2, 3 => 4)
+  end
+
+  it 'does not call to_hash on hash subclasses' do
+    my_hash = Class.new(Hash)
+
+    expect({ 3 => 4 }.merge(my_hash[1 => 2])).to eql(1 => 2, 3 => 4)
+  end
+
+  it 'returns subclass instance for subclasses' do
+    my_hash = Class.new(Hash)
+
+    expect(my_hash[1 => 2, 3 => 4].merge(1 => 2)).to be_an_instance_of(my_hash)
+    expect(my_hash[].merge(1 => 2)).to be_an_instance_of(my_hash)
+
+    expect({ 1 => 2, 3 => 4 }.merge(my_hash[1 => 2]).class).to eql Hash
+    expect({}.merge(my_hash[1 => 2]).class).to eql Hash
+  end
+
+  it 'processes entries with same order as each()' do
+    h = { 1 => 2, 3 => 4, 5 => 6, 'x' => nil, nil => 5, [] => [] }
+    merge_pairs = []
+    each_pairs = []
+    h.each_pair { |k, v| each_pairs << [k, v] }
+    h.merge(h) { |k, v1, _v2| merge_pairs << [k, v1] }
+
+    expect(merge_pairs).to eql each_pairs
+  end
 end

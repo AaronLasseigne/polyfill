0999839b69f515d13beb6f4e5ff5ae520cbe5d60
abourque72 <amejinight@gmail.com>
2020-06-02 10:41:16 -0500

    WIP

--- lib/polyfill/v2_6/matrix.rb
+++ lib/polyfill/v2_6/matrix.rb
@@ -2,19 +2,97 @@ require 'matrix'
 
 module Polyfill
   module V2_6
-    module Matrix
+    module Matrix # rubocop:disable Metrics/ModuleLength
       def []=(i, j, v)
         raise FrozenError, "can't modify frozen Matrix" if frozen?
-        (rows = check_range(i, :row)) || (row = check_int(i, :row))
-        (columns = check_range(j, :column)) || (column = check_int(j, :column))
+        check_range = lambda do |val, direction|
+          return unless val.is_a?(Range)
+          count = direction == :row ? row_count : column_count
+          canonical = (val.begin + (val.begin < 0 ? count : 0))..
+                  (val.end ? val.end + (val.end < 0 ? count : 0) - (val.exclude_end? ? 1 : 0) # rubocop:disable Style/NestedTernaryOperator, Style/MultilineTernaryOperator
+                           : count - 1)
+          unless 0 <= canonical.begin && canonical.begin <= canonical.end && canonical.end < count
+            raise IndexError, "given range #{val} is outside of #{direction} dimensions: 0...#{count}"
+          end
+          canonical
+        end
+        check_int = lambda do |val, direction|
+          count = direction == :row ? row_count : column_count
+          val = CoercionHelper.coerce_to_int(val)
+          if val >= count || val < -count
+            raise IndexError, "given #{direction} #{val} is outside of #{-count}...#{count}"
+          end
+          val
+        end
+        set_row_and_col_range = lambda do |row_range, col_range, value|
+          if value.is_a?(Matrix)
+            if row_range.size != value.row_count || col_range.size != value.column_count
+              raise StandardError, [
+                'Expected a Matrix of dimensions',
+                "#{row_range.size}x#{col_range.size}",
+                'got',
+                "#{value.row_count}x#{value.column_count}"
+              ].join(' ')
+            end
+            source = value.instance_variable_get :@rows
+            row_range.each_with_index do |row, source_row|
+              @rows[row][col_range] = source[source_row]
+            end
+          elsif value.is_a?(Vector)
+            raise StandardError, 'Expected a Matrix or a value, got a Vector'
+          else
+            value_to_set = Array.new(col_range.size, value)
+            row_range.each do |r|
+              @rows[r][col_range] = value_to_set
+            end
+          end
+        end
+        set_column_vector = lambda do |row_range, col, value|
+          value.each_with_index do |e, index|
+            r = row_range.begin + index
+            @rows[r][col] = e
+          end
+        end
+        set_row_range = lambda do |row_range, col, value|
+          if value.is_a?(Vector)
+            raise StandardError unless row_range.size == value.size
+            set_column_vector.call(row_range, col, value)
+          elsif value.is_a?(Matrix)
+            raise StandardError unless value.column_count == 1
+            value = value.column(0)
+            raise StandardError unless row_range.size == value.size
+            set_column_vector.call(row_range, col, value)
+          else
+            @rows[row_range].each { |e| e[col] = value }
+          end
+        end
+        set_col_range = lambda do |row, col_range, value|
+          value = if value.is_a?(Vector)
+                    value.to_a
+                  elsif value.is_a?(Matrix)
+                    raise StandardError unless value.row_count == 1
+                    value.row(0).to_a
+                  else
+                    Array.new(col_range.size, value)
+                  end
+          raise StandardError unless col_range.size == value.size
+          @rows[row][col_range] = value
+        end
+        set_value = lambda do |row, col, value|
+          raise StandardError, "Expected a a value, got a #{value.class}" if value.respond_to?(:to_matrix)
+
+          @rows[row][col] = value
+        end
+        (rows = check_range.call(i, :row)) || (row = check_int.call(i, :row))
+        (columns = check_range.call(j, :column)) || (column = check_int.call(j, :column))
         if rows && columns
-          set_row_and_col_range(rows, columns, v)
+          set_row_and_col_range.call(rows, columns, v)
         elsif rows
-          set_row_range(rows, column, v)
+          set_row_range.call(rows, column, v)
         elsif columns
-          set_col_range(row, columns, v)
+          set_col_range.call(row, columns, v)
         else
-          set_value(row, column, v)
+          set_value.call(row, column, v)
         end
       end
 
